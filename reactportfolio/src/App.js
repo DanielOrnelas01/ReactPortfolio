5488a9033aafe553732a1e374bca1419635d064f
dan <danielornelas01@yahoo.com>
2022-08-22 17:56:14 -0700

    fixing package and modules

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -8,15 +8,6 @@ import Resume from './components/Resume';
 import Footer from './components/Footer';
 import './App.css';
 
-import React, { useState } from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-import Footer from './components/Footer';
-import './App.css';
-
 
 function App() {
   const [currentTab, setCurrentTab] = useState("about");
