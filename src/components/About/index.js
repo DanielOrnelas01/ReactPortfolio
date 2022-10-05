99bad72b20ef96ea67c40f83a22b04b8ec356fb0
dan <danielornelas01@yahoo.com>
2022-10-05 12:29:29 -0700

    about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,7 +16,7 @@ function About() {
 			</div>
             <div className="about-me">
                 <p>
-                    Hi I'm Daniel - an aspiring full-stack web developer based out of Bakersfield, CA. My journey into tech started at the end of 2021, I took the opportunity to reinvent myself and pursue a new and challenging career in web development.
+                    Hi I'm Daniel - a full-stack web developer based out of Bakersfield, CA. My journey into tech started at the end of 2021, I took the opportunity to reinvent myself and pursue a new and challenging career in web development.
                 </p>
                 <p>
                     I'm leveraging my communications background to build a more intuitive user experience on the web. With newly developed skills in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on the MERN technology stack. I’m excited to apply my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
